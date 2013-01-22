a91b2b8519c7208ed86cea577b20921a64203aad
Soren Bjornstad <soren.bjornstad@gmail.com>
2013-01-22 08:52:48 -0600

    Set up to use a temporary file for import to Anki.

--- songcloze.py
+++ songcloze.py
@@ -4,12 +4,14 @@
 # Contact: soren.bjornstad@gmail.com
 # http://www.thetechnicalgeekery.com/anki
 
-ankipath = "/home/soren/code/anki/unstable/ankiqt/anki"
+ankipath = "/home/soren/code/anki/unstable/anki/runanki"
 default_input = ''
 default_output = ''
 default_title = ''
 tag_separator = ''
 
+import tempfile
+import os
 from sys import argv
 from subprocess import call
 
@@ -29,13 +31,13 @@ def get_data(msg):
     while not data:
         data = raw_input("%s: " %msg)
 
-    return data
+    return data.strip()
 
 def next_line(file):
     return (file.readline().rstrip())
 
-def open_anki(output_file):
-    call([ankipath, output_file])
+def open_anki(anki_file):
+    call([ankipath, anki_file.name])
 
 def main():
     print_help()
@@ -47,7 +49,7 @@ def main():
 
     # Open input and output files.
     lyrics_file = open(input_file)
-    anki_file = open(output_file, 'w')
+    anki_file = tempfile.NamedTemporaryFile('w', delete=False)
 
     # Find total lines in file for the loop.
     total_lines = len(lyrics_file.readlines())
@@ -62,7 +64,7 @@ def main():
     lyrics_second_line = next_line(lyrics_file)
     lyrics_file.seek(0)
 
-    line1 = "[First Line] (%s)\t%s\n" % (song_title, lyrics_first_line)
+    line1 = "[First Line] (%s)\t%s\n" % (title, lyrics_first_line)
     line2 = "[Beginning]<br>%s\t%s\n" % (lyrics_first_line, lyrics_second_line)
     cards_data += line1
     cards_data += line2
@@ -73,7 +75,7 @@ def main():
     current_line = next_line(lyrics_file)
 
     # Loop through the remainder of the cards.
-    i = 3 # We already did the first two cards
+    i = 3
     while i <= total_lines:
         cards_data += "%s<br>%s\t%s\n" % (context1, context2, current_line)
         context1 = context2
@@ -87,6 +89,6 @@ def main():
     lyrics_file.close()
 
     # Import file to Anki.
-    open_anki()
+    open_anki(anki_file)
 
 main()
