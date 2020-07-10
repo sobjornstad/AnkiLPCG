9dadee6d6e0e95f72f8ac4fbfc88e76b47eae7bb
Soren I. Bjornstad <contact@sorenbjornstad.com>
2020-07-10 17:30:56 -0500

    implement "Lines to recite"
    The one problem is that there is currently no indication whatsoever of
    how many lines you are expected to recite. Coming up.

--- src/gen_notes.py
+++ src/gen_notes.py
@@ -1,13 +1,17 @@
 import re
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Optional
 
 from anki.notes import Note
 
 from . import model_data as lpcg_models
 
 class PoemLine:
+    def __init__(self) -> None:
+        self.predecessor = None
+        self.successor = None
+
     def populate_note(self, note: Note, title: str, tags: List[str],
-                      context_lines: int, deck_id: int) -> None:
+                      context_lines: int, recite_lines: int, deck_id: int) -> None:
         """
         Fill the _note_ with content testing on the current line.
         """
@@ -16,11 +20,14 @@ class PoemLine:
         note['Title'] = title
         note['Sequence'] = str(self.seq)
         note['Context'] = self._format_context(context_lines)
-        note['Line'] = self.text
+        note['Line'] = self._format_text(recite_lines)
 
     def _format_context(self, context_lines: int):
         context_without_self = self._get_context(context_lines)[:-1]
         return ''.join("<p>%s</p>" % i for i in context_without_self)
+
+    def _format_text(self, recitation_lines: int):
+        return ''.join("<p>%s</p>" % i for i in self._get_text(recitation_lines))
 
     def _get_context(self, _lines: int) -> List[str]:
         """
@@ -29,18 +36,50 @@ class PoemLine:
         """
         raise NotImplementedError
 
+    def _get_text(self, _lines: int) -> List[str]:
+        """
+        Return a list of recitation lines, including the current line and
+        (lines - 1) of its successors.
+        """
+        raise NotImplementedError
+
 
 class Beginning(PoemLine):
+    """
+    A dummy node indicating the beginning of the poem. It's included only so
+    it can polymorphically have its context and sequence retrieved.
+    Attempting to do anything else with the node is an error.
+    """
     def __init__(self):
+        super().__init__()
         self.seq = 0
         self.text = "[Beginning]"
 
     def _get_context(self, _lines: int) -> List[str]:
         return [self.text]
 
+    def _get_text(self, _lines: int) -> List[str]:
+        """
+        The Beginning node has no defined successors, as it's not a line
+        we'll ever be asked to recite and thus we never need to know what its
+        text property is -- the first line we would ever be asked to recite
+        would be the following line.
+        """
+        raise AssertionError("The successors of the Beginning node are undefined.")
+
+    def populate_note(self, note: Note, title: str, tags: List[str],
+                      context_lines: int, deck_id: int) -> None:
+        raise AssertionError("The Beginning node cannot be used to populate a note.")
+
 
 class SingleLine(PoemLine):
-    def __init__(self, text: str, predecessor: 'PoemLine') -> None:
+    """
+    A single line in a typical poem. It has text, a sequence number, a
+    predecessor (possibly the Beginning node, but never None), and if it's
+    not the last line of the poem, a successor.
+    """
+    def __init__(self, text: str, predecessor: Optional['PoemLine']) -> None:
+        super().__init__()
         self.text = text
         self.predecessor = predecessor
         self.seq = self.predecessor.seq + 1
@@ -51,9 +90,16 @@ class SingleLine(PoemLine):
         else:
             return self.predecessor._get_context(lines - 1) + [self.text]
 
+    def _get_text(self, lines: int) -> List[str]:
+        if lines == 1 or self.successor is None:
+            return [self.text]
+        else:
+            return [self.text] + self.successor._get_text(lines - 1)
+
 
 class GroupedLine(PoemLine):
     def __init__(self, text: List[str], predecessor: 'PoemLine') -> None:
+        super().__init__()
         self.text_lines = text
         self.predecessor = predecessor
         self.seq = self.predecessor.seq + 1
@@ -63,6 +109,12 @@ class GroupedLine(PoemLine):
             return [self.text]
         else:
             return self.predecessor._get_context(lines - 1) + self.text_lines
+
+    def _get_text(self, lines: int) -> List[str]:
+        if lines == 1 or self.successor is None:
+            return self.text_lines
+        else:
+            return self.text_lines + self.successor._get_text(lines - 1)
 
 
 def poemlines_from_textlines(text_lines: List[str]) -> List[PoemLine]:
@@ -77,6 +129,7 @@ def poemlines_from_textlines(text_lines: List[str]) -> List[PoemLine]:
     for text_line in text_lines:
         poem_line = SingleLine(text_line, pred)
         lines.append(poem_line)
+        pred.successor = poem_line
         pred = poem_line
     return lines
 
@@ -122,7 +175,7 @@ def process_text(string: str, config: Dict[str, Any]) -> List[str]:
 
 
 def add_notes(col: Any, title: str, tags: List[str], text: List[str], deck_id: int,
-              lines_of_context: int = 2):
+              context_lines: int, group_lines: int, recite_lines: int):
     """
     Generate notes from the given title, tags, poem text, and number of
     lines of context. Return the number of notes added.
@@ -136,7 +189,7 @@ def add_notes(col: Any, title: str, tags: List[str], text: List[str], deck_id: i
     added = 0
     for line in poemlines_from_textlines(text):
         n = Note(col, col.models.byName(lpcg_models.NAME))
-        line.populate_note(n, title, tags, lines_of_context, deck_id)
+        line.populate_note(n, title, tags, context_lines, recite_lines, deck_id)
         col.addNote(n)
         added += 1
     return added
