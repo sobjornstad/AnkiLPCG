c28065695ea4fd219dbefca8bc72364ab1c12371
Soren I. Bjornstad <contact@sorenbjornstad.com>
2020-07-11 16:26:24 -0500

    provide for updating note types
    This isn't quite done, because this really needs to be a per-profile
    setting or we will get ourselves into big trouble for people with
    multiple profiles.

--- src/__init__.py
+++ src/__init__.py
@@ -15,34 +15,8 @@ if 'pytest' not in sys.modules:
     import aqt
     from aqt.qt import QAction  # type: ignore
 
-    from . import model_data as lpcg_models
     from .lpcg_dialog import LPCGDialog
-
-
-    def ensure_note_type():
-        "Create the LPCG note type if it doesn't already exist."
-        mm = aqt.mw.col.models
-        model = mm.byName(lpcg_models.NAME)
-        if model is not None:
-            # Inject night-mode update if not present.
-            if '.nightMode .cloze' not in model['css']:
-                model['css'] += ("\n\n" + lpcg_models.NIGHTMODE_CLOZE)
-            
-            # In any case, don't recreate the note type.
-            return
-
-        model = mm.new(lpcg_models.NAME)
-        for i in lpcg_models.FIELDS:
-            field = mm.newField(i)
-            mm.addField(model, field)
-        t = mm.newTemplate(lpcg_models.TEMPLATE_NAME)
-        t['qfmt'] = lpcg_models.FRONT_TEMPLATE
-        t['afmt'] = lpcg_models.BACK_TEMPLATE
-        mm.addTemplate(model, t)
-        model['css'] = lpcg_models.STYLING
-        model['sortf'] = lpcg_models.SORT_FIELD
-        mm.add(model)
-
+    from . import models
 
     def open_dialog():
         "Launch the add-poem dialog."
@@ -55,4 +29,4 @@ if 'pytest' not in sys.modules:
         aqt.mw.form.menuTools.addAction(action)
         action.triggered.connect(open_dialog)
 
-        aqt.gui_hooks.profile_did_open.append(ensure_note_type)
+        aqt.gui_hooks.profile_did_open.append(models.ensure_note_type)
---
+++ src/models.py
@@ -0,0 +1,279 @@
+"""
+trmodels.py - self-constructing definitions of Anki models (note types)
+
+Creating a note type in Anki is a procedural operation, which is inconvenient
+and difficult to read when note types need to be defined in source code
+rather than through the GUI. This module allows note types to be defined
+declaratively.
+
+It first defines TemplateData and ModelData classes which contain the logic
+for creating templates and note types, then subclasses which define the
+options and templates for each note type our application needs to create and
+manage. A framework for checking if note types exist and have the expected
+fields, and for changing between note types defined here, is also provided.
+
+These classes are a bit unusual in that they are never instantiated and have
+no instance methods or variables. The class structure is just used as a
+convenient way to inherit construction logic and fields and group related
+information together.
+
+This mini-framework was originally created for TiddlyRemember and is
+presented in slightly altered form here. The third project should combine the
+two versions and create a standardized system!
+"""
+from abc import ABC
+import inspect
+import re
+from textwrap import dedent
+from typing import Callable, Dict, Iterable, List, Optional, Tuple, Type
+import sys
+
+import aqt
+from aqt.utils import showInfo
+from anki.consts import MODEL_CLOZE
+from anki.models import Template as AnkiTemplate
+from anki.models import NoteType as AnkiModel
+
+
+class TemplateData(ABC):
+    """
+    Self-constructing definition for templates.
+    """
+    name: str
+    front: str
+    back: str
+
+    @classmethod
+    def to_template(cls) -> AnkiTemplate:
+        "Create and return an Anki template object for this model definition."
+        assert aqt.mw is not None, "Tried to use models before Anki is initialized!"
+        mm = aqt.mw.col.models
+        t = mm.newTemplate(cls.name)
+        t['qfmt'] = dedent(cls.front).strip()
+        t['afmt'] = dedent(cls.back).strip()
+        return t
+
+
+class ModelData(ABC):
+    """
+    Self-constructing definition for models.
+    """
+    name: str
+    fields: Tuple[str, ...]
+    templates: Tuple[Type[TemplateData]]
+    styling: str
+    sort_field: str
+    is_cloze: bool
+    version: str
+    upgrades: Tuple[Tuple[str, str, Callable[[AnkiModel], None]], ...]
+
+    @classmethod
+    def to_model(cls) -> Tuple[AnkiModel, str]:
+        """
+        Create and return a pair of (Anki model object, version spec)
+        for this model definition.
+        """
+        assert aqt.mw is not None, "Tried to use models before Anki is initialized!"
+        mm = aqt.mw.col.models
+        model = mm.new(cls.name)
+        for i in cls.fields:
+            field = mm.newField(i)
+            mm.addField(model, field)
+        for template in cls.templates:
+            t = template.to_template()
+            mm.addTemplate(model, t)
+        model['css'] = dedent(cls.styling).strip()
+        model['sortf'] = cls.fields.index(cls.sort_field)
+        if cls.is_cloze:
+            model['type'] = MODEL_CLOZE
+        return model, cls.version
+
+    @classmethod
+    def upgrade_from(cls, current_version: str) -> str:
+        """
+        Given that the model is at version current_version (typically stored
+        in the add-on config), run all functions possible in the updates tuple
+        of the model. The updates tuple must be presented in chronological order;
+        each element is itself a 3-element tuple:
+
+        [0] Version number to upgrade from
+        [1] Version number to upgrade to
+        [2] Function taking one argument, the model, and mutating it as required;
+            raises an exception if update failed.
+
+        Returns the new version the model is at.
+        """
+        assert aqt.mw is not None, "Tried to use models before Anki is initialized!"
+        model = aqt.mw.col.models.byName(cls.name)
+
+        at_version = current_version
+        for cur_ver, new_ver, func in cls.upgrades:
+            if at_version == cur_ver:
+                func(model)
+                at_version = new_ver
+        if at_version != current_version:
+            aqt.mw.col.models.save(model)
+        return at_version
+
+    @classmethod
+    def in_collection(cls) -> bool:
+        """
+        Determine if a model by this name exists already in the current
+        Anki collection.
+        """
+        assert aqt.mw is not None, "Tried to use models before Anki is initialized!"
+        mm = aqt.mw.col.models
+        model = mm.byName(cls.name)
+        return model is not None
+
+    @classmethod
+    def can_upgrade(cls, current_version: str) -> bool:
+        """
+        Return True if we know of a newer version of the model than supplied.
+        """
+        if cls.is_at_version(current_version):
+            return False
+        for cur_ver, _, __ in cls.upgrades:
+            if current_version == cur_ver:
+                return True
+        return False
+
+    @classmethod
+    def is_at_version(cls, current_version: str) -> bool:
+        "Return True if this model is at the version current_version."
+        return current_version == cls.version
+
+
+def upgrade_onethreeoh(mod):
+    mod['css'] += "\n\n"
+    mod['css'] += dedent("""
+        .nightMode .cloze {
+            filter: invert(85%);
+        }
+        """).strip()
+    mod['css'] = mod['css'].replace('margin-left: -30px;', '')
+
+    assert len(mod['tmpls']) == 1, "LPCG note type has extra templates!"
+    mod['tmpls'][0]['qfmt'] = mod['tmpls'][0]['qfmt'].replace(
+        '<span class="cloze">[...]</span>',
+        dedent('''
+            <div class="cloze">
+                {{#Prompt}}{{Prompt}}{{/Prompt}}
+                {{^Prompt}}[...]{{/Prompt}}
+            </div>
+        ''').strip()
+    )
+    mod['tmpls'][0]['afmt'] = mod['tmpls'][0]['afmt'].replace(
+        '<span class="cloze">{{Line}}</span>',
+        '<div class="cloze">{{Line}}</div>'
+    )
+
+    mm = aqt.mw.col.models
+    field = mm.newField("Prompt")
+    mm.addField(mod, field)
+
+
+class LpcgOne(ModelData):
+    class LpcgOneTemplate(TemplateData):
+        name = "LPCG1"
+        front = """
+            <div class="title">{{Title}} {{Sequence}}</div>
+
+            <br>
+
+            <div class="lines">
+                {{Context}}
+                <div class="cloze">
+                    {{#Prompt}}{{Prompt}}{{/Prompt}}
+                    {{^Prompt}}[...]{{/Prompt}}
+                </div>
+            </div>
+        """
+        back = """
+            <div class="title">{{Title}} {{Sequence}}</div>
+
+            <br>
+
+            <div class="lines">
+                {{Context}}
+                <div class="cloze">{{Line}}</div>
+            </div>
+        """
+
+    name = "LPCG 1.0"
+    fields = ("Line", "Context", "Title", "Sequence", "Prompt")
+    templates = (LpcgOneTemplate,)
+    styling = """
+        .card {
+            font-family: arial;
+            font-size: 20px;
+            color: black;
+            background-color: white;
+        }
+
+        p {
+            margin-top: 0px;
+            margin-bottom: 0px;
+        }
+
+        .lines {
+            text-align: left;
+            margin-left: 30px;
+            text-indent: -30px;
+            margin-right: 30px;
+        }
+
+        .cloze {
+            font-weight: bold;
+            color: blue;
+        }
+
+        .nightMode .cloze {
+            filter: invert(85%);
+        }
+
+        .title {
+            text-align: center;
+            font-size: small;
+        }
+
+        .indent {
+            margin-left: 60px;
+        }
+    """
+    sort_field = "Sequence"
+    is_cloze = False
+    version = "1.3.0"
+    upgrades = (
+        ("none", "1.3.0", upgrade_onethreeoh),
+    )
+
+
+def ensure_note_type() -> None:
+    """
+    Create or update the LPCG note type as needed.
+    """
+    assert aqt.mw is not None, "Tried to use models before Anki is initialized!"
+    conf = aqt.mw.addonManager.getConfig(__name__)
+    assert conf is not None, "Received no configuration from add-on manager!"
+    current_version = conf['systemConfigDoNotChange']['noteTypeVersion']
+    mod = LpcgOne
+
+    if not mod.in_collection():
+        model_data, new_version = mod.to_model()
+        aqt.mw.col.models.add(model_data)
+        conf['systemConfigDoNotChange']['noteTypeVersion'] = new_version
+    elif mod.can_upgrade(current_version):
+        new_version = mod.upgrade_from(current_version)
+        conf['systemConfigDoNotChange']['noteTypeVersion'] = new_version
+        aqt.mw.addonManager.writeConfig(__name__, conf)
+        showInfo("Your LPCG note type was automatically upgraded. "
+                "Please take a moment to ensure your LPCG cards "
+                "are still displaying as expected so you can restore from a backup"
+                "in the event something is not working correctly.")
+
+    assert mod.is_at_version(conf['systemConfigDoNotChange']['noteTypeVersion']), \
+        "Your LPCG model is out of date, but I couldn't find a valid upgrade path. " \
+        "You are likely to encounter issues. " \
+        "Please contact the developer for assistance resolving this problem."
+
